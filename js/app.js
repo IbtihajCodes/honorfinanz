0560c18c63abf65226a4e92e1c2776d24f431e32
IbtihajCodes <145850898+IbtihajCodes@users.noreply.github.com>
2024-08-21 18:28:32 +0500

    Only Image Slider Left

---
+++ js/app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const hamBurger = document.querySelector(".size-6");
+const sideBar = document.querySelector(".nav-ul-mobile");
+const navBar = document.querySelector("nav");
+const infoBar = document.querySelector(".information-section");
+const body = document.querySelector("body");
+const closeBtn = document.getElementById("close-btn");
+const heroPage = document.querySelector(".hero-page");
+
+hamBurger.addEventListener("click", function () {
+  sideBar.classList.remove("d-none");
+  navBar.classList.add("blur-sidebar");
+  infoBar.classList.add("blur-sidebar");
+  heroPage.classList.add("blur-sidebar");
+  body.classList.add("no-scroll");
+});
+
+closeBtn.addEventListener("click", function () {
+  sideBar.classList.add("d-none");
+  navBar.classList.remove("blur-sidebar");
+  infoBar.classList.remove("blur-sidebar");
+  heroPage.classList.remove("blur-sidebar");
+  body.classList.remove("no-scroll");
+});
