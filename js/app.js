dc9dfd1194d45aca1a70b6b109a6d1333f468351
IbtihajCodes <145850898+IbtihajCodes@users.noreply.github.com>
2024-08-22 11:44:50 +0500

    the website is complete now.

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const body = document.querySelector("body");
 const closeBtn = document.getElementById("close-btn");
 const heroPage = document.querySelector(".hero-page");
 
+// Side Bar
 hamBurger.addEventListener("click", function () {
   sideBar.classList.remove("d-none");
   navBar.classList.add("blur-sidebar");
@@ -22,4 +23,45 @@ closeBtn.addEventListener("click", function () {
   infoBar.classList.remove("blur-sidebar");
   heroPage.classList.remove("blur-sidebar");
   body.classList.remove("no-scroll");
+});
+
+// Image Slider
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const slideTrack = document.querySelector('.slide-track');
+const slides = document.querySelectorAll('.slide');
+const slideWidth = slides[0].offsetWidth + 70; // Adjusted for the margin
+
+let currentIndex = 0;
+
+prevButton.addEventListener("click", function() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = slides.length / 2 - 1;
+        slideTrack.style.transition = 'none';
+        slideTrack.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+        setTimeout(() => {
+            slideTrack.style.transition = 'transform 0.5s ease';
+            currentIndex--;
+            slideTrack.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+        }, 0);
+    } else {
+        slideTrack.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+    }
+});
+
+nextButton.addEventListener("click", function() {
+    currentIndex++;
+    if (currentIndex >= slides.length / 2) {
+        currentIndex = 0;
+        slideTrack.style.transition = 'none';
+        slideTrack.style.transform = `translateX(0)`;
+        setTimeout(() => {
+            slideTrack.style.transition = 'transform 0.5s ease';
+            currentIndex++;
+            slideTrack.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+        }, 0);
+    } else {
+        slideTrack.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+    }
 });
